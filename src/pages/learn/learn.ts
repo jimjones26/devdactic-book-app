3d177948a8dee9c02731c94abb0b14a7299beac1
jimjones26 <jimjones26@gmail.com>
2016-12-09 17:15:46 -0500

    add layout for learn page

--- src/pages/learn/learn.ts
+++ src/pages/learn/learn.ts
@@ -1,22 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { InAppBrowser } from 'ionic-native';
 
-/*
-  Generated class for the Learn page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-learn',
   templateUrl: 'learn.html'
 })
 export class LearnPage {
+  learnType = 'courses';
+  course1Url = 'http://courses.devdactic.com/p/ionic-by-doing';
+  course2Url = 'http://courses.devdactic.com/p/ionic-2-in-one-hour';
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public navCtrl: NavController) { }
 
   ionViewDidLoad() {
     console.log('Hello LearnPage Page');
+  }
+
+  public openUrl(url: string) {
+    let browser = new InAppBrowser(url, 'blank');
+    //browser.show();
   }
 
 }
